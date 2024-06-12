330182ee7ea95fd0b1410b83298afdd1f163c858
mridul <mridulsujera@gmail.com>
2024-06-12 10:01:21 +0400

    the repo has been modified to include neccesary files

--- tempCodeRunnerFile.cpp
+++
@@ -1,9 +0,0 @@
-
-                int index=getfree(parkinglot);
-                if (index>-1){
-                    parkinglot[index].fillspace(1);
-                }
-                else {
-                    cout<<"parking not free"<<endl;
-                }
-                break;
